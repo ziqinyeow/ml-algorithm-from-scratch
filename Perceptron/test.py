ace0f9abc50e6da43b3e5fecd3dbc12b441873e1
Yeow Zi Qin <ziqinyeow@gmail.com>
2022-03-05 18:54:59 +0800

    understanding perceptron

--- Perceptron/test.py
+++ Perceptron/test.py
@@ -19,23 +19,23 @@ X_train, X_test, y_train, y_test = train_test_split(
 p = Perceptron(lr=0.01, n_iters=1000)
 p.fit(X_train, y_train)
 predictions = p.predict(X_test)
-
+print(y_train)
 print(accuracy(y_test, predictions))
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 plt.scatter(X_train[:, 0], X_train[:, 1], marker='o', c=y_train)
 
-# x0_1 = np.amin(X_train[:, 0])
-# x0_2 = np.amax(X_train[:, 0])
+x0_1 = np.amin(X_train[:, 0])
+x0_2 = np.amax(X_train[:, 0])
 
-# x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
-# x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
+x1_1 = (-p.weights[0] * x0_1 - p.bias) / p.weights[1]
+x1_2 = (-p.weights[0] * x0_2 - p.bias) / p.weights[1]
 
-# ax.plot([x0_1, x0_2], [x1_1, x1_2], "k")
+ax.plot([x0_1, x0_2], [x1_1, x1_2], "k")
 
-# ymin = np.amin(X_train[:, 1])
-# ymax = np.amax(X_train[:, 1])
-# ax.set_ylim([ymin - 3, ymax + 3])
+ymin = np.amin(X_train[:, 1])
+ymax = np.amax(X_train[:, 1])
+ax.set_ylim([ymin - 3, ymax + 3])
 
 plt.show()
